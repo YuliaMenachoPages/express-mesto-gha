88ec4ffffdc3b595622e97a52b1c3ed4919ef2c8
YuliaMenachoPages <yumenachopages@gmail.com>
2023-06-20 14:48:43 +0300

    mesto_13_bugfix_11_review_1

--- models/user.js
+++ models/user.js
@@ -3,28 +3,28 @@ const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный URL',
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректный URL',
+      },
     },
   },
-},
-{ versionKey: false },
+  { versionKey: false },
 );
 
 module.exports = mongoose.model('user', userSchema);
